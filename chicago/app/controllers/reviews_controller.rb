1bff079bccb7fa50091a0888b252f6abfe5ad9b2
lsliusen <lsliusen1986@gmail.com>
2015-04-30 15:57:52 -0500

    Sen Liu's submission for homework4.

--- chicago/app/controllers/reviews_controller.rb
+++ chicago/app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
 
   def create
-    r = Review.new
-    r.place_id = params["place_id"]
-    r.rating = params["rating"]
-    r.desc = params["desc"]
-    r.save
-    redirect_to "/places/#{r.place_id}"
+    review = Review.new
+    review.place_id = params["place_id"]
+    review.rating = params["rating"]
+    review.desc = params["desc"]
+    review.save
+    redirect_to "/places/#{review.place_id}"
   end
 
 end
--- chicago/config/routes.rb
+++ chicago/config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
 
   get '/places' => 'places#index'
   get '/places/new' => 'places#new'
-  get '/places/submit_new' => 'places#submitted'
-  get '/places/:id' => 'places#show'
-  get '/places/:id/delete' => 'places#delete'
-  get '/places/:id/edit' => 'places#edit'
-  get '/places/:id/submit_edit' => 'places#update'
+  post '/places' => 'places#create'
 
-  get '/reviews/:place_id/submit_new'  => 'reviews#create'
+  get '/places/:id' => 'places#show', :as => 'place'
+
+  delete '/places/:id' => 'places#destroy'
+
+  get '/places/:id/edit' => 'places#edit'
+  patch '/places/:id' => 'places#update'
+
+  post '/reviews/:place_id'  => 'reviews#create'
 
 end
